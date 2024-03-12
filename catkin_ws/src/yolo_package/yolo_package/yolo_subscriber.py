435e657745476855bcb702d05f2050b192f04cff
김태범 <ktb5057@gmail.com>
2024-03-12 10:55:45 +0900

    [#82][EM FEAT] Yolo publisher 추가

---
+++ catkin_ws/src/yolo_package/yolo_package/yolo_subscriber.py
@@ -0,0 +1,37 @@
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
+
+
+class YoloSubscriber(Node):
+
+    def __init__(self):
+        super().__init__('minimal_subscriber')
+        self.subscription = self.create_subscription(
+            String,
+            'captured_object',
+            self.listener_callback,
+            10)
+        self.subscription  # prevent unused variable warning
+
+    def listener_callback(self, msg):
+        self.get_logger().info('Yolo Subscriber heards: "%s"' % msg.data)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    yolo_subscriber = YoloSubscriber()
+
+    rclpy.spin(yolo_subscriber)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    yolo_subscriber.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
