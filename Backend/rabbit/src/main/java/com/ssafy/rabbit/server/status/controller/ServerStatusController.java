3dae453e81c8e3ee333ff0b16858a92d514b6e6d
연정흠 <madirony52@kakao.com>
2024-03-14 14:10:42 +0900

    #S10P22A209-124 [BE FEAT] rabbitMQ WAS 프로젝트 생성

---
+++ Backend/rabbit/src/main/java/com/ssafy/rabbit/server/status/controller/ServerStatusController.java
@@ -0,0 +1,12 @@
+package com.ssafy.rabbit.server.status.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ServerStatusController {
+    @GetMapping("/api/v1/rabbitTest")
+    public String testAPI() {
+        return "✅ RabbitMQ Service is up and running!";
+    }
+}
