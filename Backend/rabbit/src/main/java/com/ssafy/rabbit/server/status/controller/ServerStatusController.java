7d76ef9a119b86f5226c67b62b466cd01314c58d
연정흠 <madirony52@kakao.com>
2024-03-19 15:43:31 +0900

    #S10P22A209-124 [BE TEST] RabbitWAS 유기

--- Backend/rabbit/src/main/java/com/ssafy/rabbit/server/status/controller/ServerStatusController.java
+++ Backend/rabbit/src/main/java/com/ssafy/rabbit/server/status/controller/ServerStatusController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ServerStatusController {
-    @GetMapping("/api/v1/rabbitTest") //test
+    @GetMapping("/rabbitTest") //test
     public String testAPI() {
         return "✅ RabbitMQ Service is up and running!";
     }
