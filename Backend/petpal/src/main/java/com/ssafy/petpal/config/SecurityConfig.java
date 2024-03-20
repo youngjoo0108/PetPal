242dbe0ea83351c983d546529316e13e32f00dfa
연정흠 <madirony52@kakao.com>
2024-03-20 10:52:07 +0900

    #S10P22A209-124 [BE TEST] RabbitMQ properties 암호화

--- Backend/petpal/src/main/java/com/ssafy/petpal/config/SecurityConfig.java
+++ Backend/petpal/src/main/java/com/ssafy/petpal/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.ssafy.petpal.security.config;
+package com.ssafy.petpal.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +9,14 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
-public class TmpSecurityConfig {
+public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/api/v1/test").permitAll()  // /api/v1/test에 대한 접근을 모두에게 허용
+                        .requestMatchers("/api/ws/**").permitAll()
                         .anyRequest().authenticated()  // 나머지 요청에 대해서는 인증을 요구
                 )
                 .httpBasic(withDefaults())  // HTTP Basic 인증 활성화
