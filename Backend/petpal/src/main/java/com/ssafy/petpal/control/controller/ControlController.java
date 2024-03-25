7de494c2222c654dc61972161bfc1fea31c33886
연정흠 <madirony52@kakao.com>
2024-03-25 10:25:18 +0900

    #S10P22A209-146 [BE TEST] 바이트 배열 테스트

--- Backend/petpal/src/main/java/com/ssafy/petpal/control/controller/ControlController.java
+++ Backend/petpal/src/main/java/com/ssafy/petpal/control/controller/ControlController.java
@@ -1,31 +1,37 @@
 package com.ssafy.petpal.control.controller;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.petpal.control.dto.ControlDto;
-import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Controller;
 
 @Controller
-@RequiredArgsConstructor
 public class ControlController {
     private final RabbitTemplate rabbitTemplate;
+    private final ObjectMapper objectMapper;
 
     private static final Logger logger = LoggerFactory.getLogger(ControlController.class);
-
     private static final String CONTROL_QUEUE_NAME = "control.queue";
     private static final String CONTROL_EXCHANGE_NAME = "control.exchange";
+
+    @Autowired
+    public ControlController(RabbitTemplate rabbitTemplate, ObjectMapper objectMapper) {
+        this.rabbitTemplate = rabbitTemplate;
+        this.objectMapper = objectMapper;
+    }
 
     @MessageMapping("control.message.{userId}")
     public void sendMessage(@Payload ControlDto controlDto, @DestinationVariable String userId) throws JsonProcessingException {
         logger.info("Received message: {}", controlDto);
-        rabbitTemplate.convertAndSend(CONTROL_EXCHANGE_NAME, "user." + userId, controlDto);
+        rabbitTemplate.convertAndSend(CONTROL_EXCHANGE_NAME, "user." + userId, objectMapper.writeValueAsString(controlDto));
     }
 
     @RabbitListener(queues = CONTROL_QUEUE_NAME)
