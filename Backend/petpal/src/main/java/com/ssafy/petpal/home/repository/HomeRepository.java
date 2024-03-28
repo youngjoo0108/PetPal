bbf155caa6e5b0b4a7542dd7268fb13166a93e73
김봉균 <qhdrnak@hanyang.ac.kr>
2024-03-28 14:25:16 +0900

    #S10P22A209-218 [BE TEST] Change Home API

--- Backend/petpal/src/main/java/com/ssafy/petpal/home/repository/HomeRepository.java
+++ Backend/petpal/src/main/java/com/ssafy/petpal/home/repository/HomeRepository.java
@@ -3,5 +3,8 @@ package com.ssafy.petpal.home.repository;
 import com.ssafy.petpal.home.entity.Home;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface HomeRepository extends JpaRepository<Home,Long> {
+    List<Home> findByUserId(Long userId);
 }
--- Backend/petpal/src/main/java/com/ssafy/petpal/home/service/HomeService.java
+++ Backend/petpal/src/main/java/com/ssafy/petpal/home/service/HomeService.java
@@ -1,17 +1,33 @@
 package com.ssafy.petpal.home.service;
 
+import com.ssafy.petpal.home.dto.HomeRequestDTO;
+import com.ssafy.petpal.home.entity.Home;
 import com.ssafy.petpal.home.repository.HomeRepository;
+import com.ssafy.petpal.user.entity.User;
+import com.ssafy.petpal.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class HomeService {
 
     private final HomeRepository homeRepository;
+    private final UserRepository userRepository;
 
+    public void createHome(HomeRequestDTO homeRequestDTO) {
+        User user = userRepository.findByUserId(homeRequestDTO.getUserId())
+                .orElseThrow(IllegalArgumentException::new);
+        Home home = Home.builder()
+                .homeNickname(homeRequestDTO.getHomeNickname())
+                .user(user)
+                .build();
+        homeRepository.save(home);
+    }
 
-    public void createHome() {
-
+    public List<Home> fetchAllByUserId(Long userId) {
+        return homeRepository.findByUserId(userId);
     }
 }
