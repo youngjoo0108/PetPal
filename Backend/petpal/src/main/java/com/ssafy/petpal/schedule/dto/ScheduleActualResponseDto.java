8f104b3ed29095f0192a006eec33ce25a552861c
김봉균 <qhdrnak@hanyang.ac.kr>
2024-04-01 10:05:38 +0900

    #S10P22A209-218 [BE REFACTOR] change SchduleResponseDTO

---
+++ Backend/petpal/src/main/java/com/ssafy/petpal/schedule/dto/ScheduleActualResponseDto.java
@@ -0,0 +1,30 @@
+package com.ssafy.petpal.schedule.dto;
+
+import com.ssafy.petpal.object.dto.Location;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import org.locationtech.jts.geom.Point;
+
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@Getter
+public class ScheduleActualResponseDto {
+
+    private Long applianceId;
+
+    private String applianceType;
+    private Location coordinate;
+    private String applianceUUID;
+
+    private String roomName;
+
+    private String day;
+    private LocalTime time;
+    private String taskType;
+
+    private boolean isRepeat;
+    private boolean isActive; // 스케줄 자체의 상태
+}
