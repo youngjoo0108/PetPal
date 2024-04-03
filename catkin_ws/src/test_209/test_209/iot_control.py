13a41f7abb7bd7568b572c3b7a8600cf207767a9
김승배 <ksshc@snu.ac.kr>
2024-04-03 12:41:56 +0900

    [#132][EM FIX] 프론트 통신 문제 수정

--- catkin_ws/src/test_209/test_209/iot_control.py
+++ catkin_ws/src/test_209/test_209/iot_control.py
@@ -2,7 +2,6 @@ import rclpy, json
 from rclpy.node import Node
 from std_msgs.msg import String, Int32
 import numpy as np
-from ssafy_bridge.iot_udp import iot_udp
 from nav_msgs.msg import Odometry, Path
 from ssafy_msgs.msg import IotInfo, IotCmd
 from geometry_msgs.msg import PoseStamped
@@ -11,17 +10,16 @@ class iotControl(Node):
 
     def __init__(self):
         super().__init__('iot_control')
-
+        
         self.iot_sub = self.create_subscription(IotInfo, '/iot', self.iot_callback, 10)
         self.odom_sub = self.create_subscription(Odometry, '/odom', self.odom_callback, 1)
         self.iot_cmd_sub = self.create_subscription(IotCmd, '/iot_cmd', self.iot_cmd_callback, 10)
         self.goal_pub = self.create_publisher(PoseStamped, 'goal_pose', 10)
         self.request_pub = self.create_publisher(String, 'request', 10)
-        self.err_pub = self.create_publisher(Int32, 'err', 10)
         self.data_pub = self.create_publisher(String, '/to_server/data', 10)
+        self.iot_user_pub = self.create_publisher(IotCmd, 'iot_user', 10)
         self.timer = self.create_timer(0.1, self.timer_callback)
 
-        self.iot_control = iot_udp()
         self.devices = {}
         self.device_uid = []
         self.now_device = None
@@ -38,15 +36,16 @@ class iotControl(Node):
         self.is_state_change = True
 
         full_path = 'C:\\Users\\SSAFY\\Desktop\\iot.txt'
-        self.f = open(full_path, 'r+')
+        f = open(full_path, 'r')
 
-        lines = self.f.readlines()
-        self.f.seek(0,2)
+        lines = f.readlines()
 
         for data in lines:
             line_data = data.split()
             self.devices[line_data[0]] = (line_data[1], line_data[2])
             self.device_uid.append(line_data[0])
+        
+        f.close()
 
     def iot_callback(self, msg):
         self.iot_msg = msg
@@ -74,7 +73,7 @@ class iotControl(Node):
                     y = self.odom_msg.pose.pose.position.y
                     self.devices[self.iot_msg.uid] = (x, y)
                     self.device_uid.append(self.iot_msg.uid)
-                    self.f.write('{0} {1} {2}\n'.format(self.iot_msg.uid, x, y))
+                    # self.f.write('{0} {1} {2}\n'.format(self.iot_msg.uid, x, y))
                 
                 temp = {
                     'type' : 'REGISTER',
@@ -91,13 +90,14 @@ class iotControl(Node):
 
 
     def timer_callback(self):
-        if self.is_state_change == False:
 
+        if self.is_state_change == False:
             if self.now_device == self.iot_msg.uid:
-                err_msg = Int32()
-                err_msg.data = 0
-                self.err_pub.publish(err_msg)
-                self.iot_control.user_cmd(self.now_device, self.cmd)
+                msg = IotCmd()
+                msg.iot_uuid = self.now_device
+                msg.control_action = self.cmd
+                self.iot_user_pub.publish(msg)
+                # self.iot_control.user_cmd(self.now_device, self.cmd)
                 self.is_state_change = True
                 self.now_device = None
 
@@ -112,9 +112,6 @@ class iotControl(Node):
                 self.request_msg.data = "iot_off"
                 self.request_pub.publish(self.request_msg)
             else:
-                err_msg = Int32()
-                err_msg.data = 1
-                self.err_pub.publish(err_msg)
                 goal = self.devices[self.now_device]
                 #print(goal)
                 self.goal_msg.pose.position.x = float(goal[0])
@@ -128,16 +125,25 @@ class iotControl(Node):
                 self.goal_msg.header.stamp = rclpy.clock.Clock().now().to_msg()
                 self.goal_pub.publish(self.goal_msg)
 
+    def save_iot(self):
+        full_path = 'C:\\Users\\SSAFY\\Desktop\\iot.txt'
+        f = open(full_path, 'w')
+        
+        temp = ''
+        for key,value in self.devices.items():
+            temp += '{0} {1} {2}\n'.format(key, value[0], value[1])
+        f.write(temp)
+        f.close()
 
 def main(args=None):
     rclpy.init(args=args)
+    device_control = iotControl()
     try :
-        device_control = iotControl()
         rclpy.spin(device_control)
         device_control.destroy_node()
         rclpy.shutdown()
     except :
-        device_control.f.close()
+        device_control.save_iot()
 
 if __name__ == '__main__':
     main()
