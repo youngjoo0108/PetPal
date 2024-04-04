ddb1a059e76c4fa3e994e91476d0d19f3ba16174
김봉균 <qhdrnak@hanyang.ac.kr>
2024-04-04 09:57:35 +0900

    #S10P22A209-225 [BE TEST] Change expire time for 3h

--- Backend/petpal/src/main/java/com/ssafy/petpal/schedule/entity/Schedule.java
+++ Backend/petpal/src/main/java/com/ssafy/petpal/schedule/entity/Schedule.java
@@ -7,6 +7,7 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -14,6 +15,7 @@ import java.time.LocalTime;
 
 @Entity(name = "Schedules")
 @Getter
+@Setter
 @NoArgsConstructor
 public class Schedule extends BaseEntity {
 
